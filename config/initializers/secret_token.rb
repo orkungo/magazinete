8de48769e7acc193910f83c4365ec8a77c0882cf
orkun <orkun@orkun-virtual-machine.(none)>
2012-09-14 11:05:28 +0200

    First commt

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Magazinete::Application.config.secret_token = '38011e623204799cbabaddf54922263bb1b21d5384d1c74077161d7cc6cab90478536f84b7cba48f3c76996a2fc75334c16019594e133767061e7cf0cf723d57'
